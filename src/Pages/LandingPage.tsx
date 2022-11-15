2e4771a3a708c54bfc14d51941c6510d333d3c35
Phillip8829 <phillipyates12@hotmail.com>
2022-11-15 11:05:10 -0600

    Finished Front End Look

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import {Link} from "react-router-dom";
 
 export default function LandingPage() {
 
     return(
-        <Box justifyContent={'center'} display={'flex'}>
-           <img  src={"/cat.gif"} />
-            <Link to={"/report"}>
-                <Button variant={"contained"}>Report Submit Page</Button>
-
-            </Link>
+        <Box sx={{ paddingLeft: 2, borderBottom: 1, textAlign: "Center" }}>
+           <img  src={"/cat.gif"}  alt={""}/>
+            <div className={"Links"} style={{backgroundColor: "yellow" , padding: 30, color: "Black"}}>
+                <img style={{backgroundColor: "black", padding: 5}} src={"https://afcwebsite.blob.core.usgovcloudapi.net/uploads/assets/sf_logo_1c_776c839b0c.svg"}/>
+                <div>
+                    <Link  style={{color: "inherit", padding: 20}}  to={"/"}>Home Page</Link>
+                    <Link style={{color: "inherit", padding: 20}} to={"/Report"}>Submit SIR Report</Link>
+                    <Link style={{color: "inherit", padding: 20}} to={"/SupervisorView"}>View All SIR Report</Link>
+                </div>
+                <img  src={"/cat.gif"}  alt={""}/>        <img  src={"/cat.gif"}  alt={""}/>         <img  src={"/cat.gif"}  alt={""}/>
+                <img  src={"/cat.gif"}  alt={""}/>        <img  src={"/cat.gif"}  alt={""}/>
+            </div>
         </Box>
 
     )
